6362c02756b7d6c966032241dfbe4c1f35749055
Ryan Hurst <r.hurst@student.uq.edu.au>
2025-06-14 20:02:32 +1000

    Made the initial snail and player, and made them move using the rectangle class.

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from sys import exit 
+
+pygame.init() #Init Pygame
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Ryan's Game")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha() #convert_alpha() makes it run faster
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
+
+#EVENT LOOP
+while True:
+    #Draw and update everything
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300, 50))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+        
+    screen.blit(snail_surface, snail_rect)
+
+    screen.blit(player_surf, player_rect)
+    
+
+    pygame.display.update()
+    clock.tick(60) #60 FPS
+
